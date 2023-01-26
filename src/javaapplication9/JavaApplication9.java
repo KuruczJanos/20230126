0d80d39c9f4db8fa6fa1a16bdd25d72b40566501
Kurucz <kuruczjanoshivatalos@gmail.com>
2023-01-26 19:26:42 +0100

    szünet

--- src/javaapplication9/JavaApplication9.java
+++ src/javaapplication9/JavaApplication9.java
@@ -1,20 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaapplication9;
+package javaapplication9;// Csigabiga program
 
-/**
- *
- * @author diak
- */
-public class JavaApplication9 {
+public class JavaApplication9 {//Osztály kezdete
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static void feltolt(int[][] t) {
+        for (int j = 0; j < t.length; j++) {
+            for (int i = 0; i < t[0].length; i++) {
+                t[j][i] = j * t.length + i;
+
+            }
+        }
     }
-    
-}
+
+    static void kiirtomb(int[][] t) {
+        for (int j = 0; j < t.length; j++) {
+            for (int i = 0; i < t[0].length; i++) {
+                System.out.print(t[j][i] + ",");
+
+            }
+        }
+    }
+
+    static void kiirspiralban(int[][] t) {
+        do {
+            int elemszam = 0, x = 0, y = 0, sor = 0, oszlop = 0;
+            elemszam = t.length - 1;
+            for (int i = 0; i < 4; i++) {
+                switch (i % 4) {
+                    case 0:
+                        x = 0;
+                        y = 1;
+                        break;
+                    case 1:
+                        x = 1;
+                        y = 0;
+                        break;
+                    case 2:
+                        x = 0;
+                        y = -1;
+                        break;
+                    case 3:
+                        x = -1;
+                        y = 0;
+                        break;
+                    default:
+                        System.out.println("Nagyon béna vagyok!!!");
+                }
+                for (int j = 0; j < elemszam; j++) {
+                    System.out.print(t[sor][oszlop] + ",");
+                    sor = sor + x;
+                    oszlop = oszlop + y;
+
+                }
+
+            }
+            elemszam = 2;
+            if (elemszam == 0) {
+                elemszam = 1;
+            }
+
+        } while (elemszam > 0);
+    }
+
+    public static void main(String[] args) {//Forráskód eleje
+        int n = 6;
+        int[][] tomb = new int[n][n];
+        feltolt(tomb);
+        kiirtomb(tomb);
+        kiirspiralban(tomb);
+
+    }//Forráskód vége
+}//Osztály vége
